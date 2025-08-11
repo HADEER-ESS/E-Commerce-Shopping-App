ace362e69d6ab7b8514f53049f6c1b7f2ab070f4
hadeer <hadeeressam21@gmail.com>
2025-08-11 22:48:16 +0300

    initialize the Details screen to display the product data and add to cart

--- src/screens/DetailsScreen.tsx
+++ src/screens/DetailsScreen.tsx
@@ -1,12 +1,58 @@
 import React from 'react'
-import { View } from 'react-native'
+import { Dimensions, FlatList, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Products } from '../data/products'
+import MainStyles from '../constant/styles'
 
-const DetailsScreen = () => {
+const width = Dimensions.get("screen").width
+
+const DetailsScreen = ({ route }) => {
+    const { product_id } = route.params
+    const product = Products.find((pro) => pro.id === product_id)
     return (
-        <View>
+        <ScrollView style={MainStyles.screenContainer}>
+            <FlatList
+                horizontal={true}
+                data={product?.images}
+                showsHorizontalScrollIndicator={false}
+                keyExtractor={(_, index) => `item num ${index}`}
+                renderItem={({ item }) => <Image style={styles.imageStyle} source={item} />}
+            />
+            {/* white layer with border radius in top left and right */}
+            <View>
+                <Text>{product?.name}</Text>
+                {/* specification */}
+                <FlatList
+                    data={product?.specifications}
+                    keyExtractor={(_, idx) => `no_ ${idx}`}
+                    renderItem={({ item }) =>
+                        <View>
+                            <Text>{item?.label}</Text>
+                            <Text>{item?.value}</Text>
+                        </View>
+                    } />
+                <Text>{product?.details_desc}</Text>
+                {/* Horizontal item with (PRICE) (Add To Cart) */}
+                <View>
+                    <Text>Price {product?.price} EGP</Text>
+                    {/* if first time selected => BTN (Add to cart) */}
+                    {/* else => BTN (-   1   +) */}
+                    <TouchableOpacity>
+                        <Text>Add To Cart</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
 
-        </View>
+        </ScrollView>
     )
 }
 
 export default DetailsScreen
+
+const styles = StyleSheet.create({
+    imageStyle: {
+        resizeMode: 'contain',
+        width: width * 0.9,
+        height: width * 0.6,
+        marginRight: 10
+    }
+})
