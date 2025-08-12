50e08159d63756d75e072371effe6c5bb88f1fa6
hadeer <hadeeressam21@gmail.com>
2025-08-12 20:54:07 +0300

    add sharable Empty Screen State

---
+++ src/componant/EmptyStateScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Image, Text, View } from 'react-native'
+
+const EmptyStateScreen = () => {
+    return (
+        <View>
+            <Image />
+            <Text></Text>
+        </View>
+    )
+}
+
+export default EmptyStateScreen
