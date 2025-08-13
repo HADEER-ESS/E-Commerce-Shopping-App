f7e033834ca3f0386d70a2644aada4b81d9d83d7
hadeer <hadeeressam21@gmail.com>
2025-08-13 13:17:53 +0300

    create the cart empty state, and add a badge to show the cart total product

--- src/componant/EmptyStateScreen.tsx
+++ src/componant/EmptyStateScreen.tsx
@@ -1,13 +1,39 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import { useTheme } from '../provider/ThemeProvider'
+import ActionBtn from './ActionBtn'
+import { useNavigation } from '@react-navigation/native'
 
 const EmptyStateScreen = () => {
+    const { theme } = useTheme()
+    const navigation = useNavigation()
     return (
-        <View>
-            <Image />
-            <Text></Text>
+        <View style={styles.viewContainer}>
+            <Image style={styles.imageStyle} source={require("../../assets/empty_state.png")} />
+            <Text style={[styles.emptyText, { color: theme.black }]}>
+                Your Cart is Empty
+            </Text>
+            <ActionBtn text={"Add items"} fun={() => navigation.goBack()} />
         </View>
     )
 }
 
 export default EmptyStateScreen
+
+const styles = StyleSheet.create({
+    viewContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageStyle: {
+        resizeMode: 'contain',
+        width: '45%',
+        height: '50%',
+    },
+    emptyText: {
+        fontSize: 24,
+        textAlign: 'center',
+        marginVertical: 24
+    }
+})
--- src/componant/Header.tsx
+++ src/componant/Header.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/native'
 import React, { useState } from 'react'
-import { Image, StyleSheet, Switch, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Switch, Text, TouchableOpacity, View } from 'react-native'
+import { useTheme } from '../provider/ThemeProvider'
 
 type headerProps = {
     cartCount: number
@@ -8,21 +9,22 @@ type headerProps = {
 
 const Header = ({ cartCount }: headerProps) => {
     const navigation = useNavigation()
-    const [isEnabled, setIsEnabled] = useState<boolean>(true);
-    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+    const { toggleTheme, mode, theme } = useTheme()
     return (
         <View style={styles.headerContainer}>
-            {/* Theme Switcher */}
             <Switch
                 trackColor={{ false: '#767577', true: '#81b0ff' }}
-                thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+                thumbColor={mode === 'light' ? '#f5dd4b' : '#f4f3f4'}
                 ios_backgroundColor="#3e3e3e"
-                onValueChange={toggleSwitch}
-                value={isEnabled}
+                onValueChange={toggleTheme}
+                value={mode === 'light'}
             />
             {/* Cart  with badge to display the count of selected item*/}
             <TouchableOpacity onPress={() => navigation.navigate("cart")}>
-                <Image style={styles.imageStyle} source={require("../../assets/products/cart.png")} />
+                <Image style={styles.imageStyle} source={require("../../assets/cart.png")} />
+                <View style={[styles.floatingCartCountView, { backgroundColor: theme.primary }]}>
+                    <Text style={styles.cartCountText}>{cartCount}</Text>
+                </View>
             </TouchableOpacity>
         </View>
     )
@@ -43,5 +45,18 @@ const styles = StyleSheet.create({
         resizeMode: 'contain',
         width: 35,
         height: 45
+    },
+    floatingCartCountView: {
+        position: 'absolute',
+        top: 30,
+        right: -18,
+        borderRadius: 20,
+        paddingHorizontal: 8,
+        paddingVertical: 4
+    },
+    cartCountText: {
+        color: '#ffffff',
+        fontSize: 12,
+        fontWeight: 'bold'
     }
 })
